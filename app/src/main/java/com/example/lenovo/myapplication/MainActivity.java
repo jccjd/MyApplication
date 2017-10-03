9f9d9e4b61372d0fceeb2e1a7a209c955a1e10de
jccjd <1340944538@qq.com>
2017-10-03 19:56:19 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.lenovo.myapplication;
 
+import android.app.Activity;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("hell earth");
+
     }
 }
