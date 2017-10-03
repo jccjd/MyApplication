3d6aa17bad637fba7727475cdda9633f06d5aa12
jccjd <1340944538@qq.com>
2017-10-03 20:27:13 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -6,20 +6,31 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private TextView textView;
-
+    private Button button;
+    int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
-        textView.setText("hell earth");
-        textView.setBackgroundColor(Color.BLUE);
+        button = (Button)findViewById(R.id.button);
+        ButtonListener buttonListener = new ButtonListener();
+        button.setOnClickListener(buttonListener);
+    }
+    class ButtonListener implements View.OnClickListener {
 
+        @Override
+        public void onClick(View v) {
+            cont++;
+            textView.setText(cont + "");
+        }
     }
 }
