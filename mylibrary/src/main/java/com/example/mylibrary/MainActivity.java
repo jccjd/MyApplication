e382b1d3236c735336d9520ec207a6c9967f6a92
jccjd <1340944538@qq.com>
2017-10-03 21:05:07 +0800

    updata

--- mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
+++ mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
@@ -8,25 +8,9 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private TextView textView;
-    private Button button;
-    int cont = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
-        button = (Button)findViewById(R.id.button);
-        ButtonListener buttonListener = new ButtonListener();
-        button.setOnClickListener(buttonListener);
-    }
-    class ButtonListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            cont++;
-            textView.setText(cont + "");
-        }
+        setContentView(R.layout.layout);
     }
 }
