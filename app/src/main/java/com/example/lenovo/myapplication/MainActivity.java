b288a14481a842309d3ea499b9a2c5f4d875238c
jccjd <1340944538@qq.com>
2017-10-05 19:45:42 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -8,14 +8,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private RadioGroup radioGroup;
+    private RadioButton maleradioButton;
+    private RadioButton famaleradioBUtton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radioGroup = (RadioGroup) findViewById(R.id.radio);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            }
+        });
+
+        maleradioButton = (RadioButton) findViewById(R.id.button);
+        famaleradioBUtton = (RadioButton) findViewById(R.id.button2);
 
     }
 
