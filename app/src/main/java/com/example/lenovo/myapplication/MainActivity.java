d76d60c966ae2d78e69c99bb3d43ccc530f5c4d4
jccjd <1340944538@qq.com>
2017-10-05 23:47:04 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -7,10 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.*;
+
 /**
 * 单选按钮
 * */
@@ -19,11 +17,13 @@ public class MainActivity extends Activity {
     private RadioGroup radioGroup;
     private RadioButton maleradioButton;
     private RadioButton famaleradioBUtton;
-
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageid);
         radioGroup = (RadioGroup) findViewById(R.id.radio);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
