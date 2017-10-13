d8a861ef1edd42c8211a9fb3c81bfe67db0b7eeb
jccjd <1340944538@qq.com>
2017-10-14 01:17:56 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -9,17 +9,28 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 /**
 * 单选按钮
 * */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private Button sign_in_button;
+    private Button sign_out_button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        sign_in_button = (Button) findViewById(R.id.sign_in_button);
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "成功登陆", LENGTH_LONG).show();
+            }
+        });
 
     }
 
