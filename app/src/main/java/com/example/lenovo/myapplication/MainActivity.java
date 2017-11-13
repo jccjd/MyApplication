575406d95751a38046dda35a59865243c33a9d1b
jccjd <1340944538@qq.com>
2017-11-13 14:49:02 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.lenovo.myapplication;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 
