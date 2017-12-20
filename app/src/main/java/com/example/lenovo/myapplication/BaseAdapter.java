580323c09452070e2e487c04b1932f3330d0c2f8
jccjd <1340944538@qq.com>
2017-12-20 15:21:31 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/BaseAdapter.java
+++ app/src/main/java/com/example/lenovo/myapplication/BaseAdapter.java
@@ -2,11 +2,8 @@ package com.example.lenovo.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,26 +12,31 @@ import java.util.Map;
 
 public class BaseAdapter extends AppCompatActivity {
     private ListView listView;
-    private List<Map<String,Object>> dataList;
+    private List<Map<String, Object>> mDataList;
     private SimpleAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_item);
+        setContentView(R.layout.base_adapter);
+
         initData();
         listView = (ListView) findViewById(R.id.list_view);
-        mAdapter = new SimpleAdapter(this,dataList,R.layout.list_item,new String []{"img",
-                "title","major_material",
+        mAdapter = new SimpleAdapter(this, mDataList, R.layout
+                .list_item, new String[]{"img", "title", "major_material",
                 "minor_material", "price","pick"}, new int[]{R
                 .id.img_menu, R.id.tv_title, R.id.tv_major_material, R.id.tv_minor_material, R.id
                 .tv_price});
         listView.setAdapter(mAdapter);
+
     }
+
     public void initData() {
-        int[] images = {R.drawable.shuizhuroupian,R.drawable.suanlajidantang,
-                R.drawable.xihucuyu,R.drawable.yuxiangrousi,};
-        String[] titles = {"宫保鸡丁","水煮肉片","西湖醋鱼","鱼香肉丝","酸辣鸡蛋汤",};
-        String[] price = {"29.9","39.9","38.9","26.9","16.9"};
+        int[] images = {R.drawable.gongbaojiding, R.drawable.shuizhuroupian,
+                R.drawable.xihucuyu, R.drawable.yuxiangrousi,
+                R.drawable.suanlajidantang};
+        String[] titles = {"宫保鸡丁", "水煮肉片", "西湖醋鱼", "鱼香肉丝", "酸辣鸡蛋汤"};
+        String[] price = {"￥29.9", "￥39.0", "￥38.9", "￥26.9", "￥16.9"};
         String[] majorMaterial = {"主料：鸡胸肉 黄瓜 胡萝卜 花生米",
                 "主料：猪里脊肉 大白菜",
                 "主料：草鱼",
@@ -54,17 +56,15 @@ public class BaseAdapter extends AppCompatActivity {
                         "Sauce）是一道特色传统名菜，以鱼香调味而得名，属川菜。相传灵感来自老菜泡椒肉丝，民国年间由四川籍厨师创制而成。",
                 "酸辣鸡蛋汤是一款简单的汤类美食，主要原料有猪肉50克、胡萝卜、竹笋各50克等。逢年过节，或寒冬腊月，煮上一锅热腾腾的酸辣鸡蛋汤，暖身提神，回味无穷"};
 
-        dataList = new ArrayList<>();
+        mDataList = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            Map<String,Object> map = new HashMap<>();
-            map.put("img",images[i]);
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", images[i]);
             map.put("title", titles[i]);
             map.put("major_material", majorMaterial[i]);
             map.put("minor_material", minorMaterial[i]);
             map.put("price", price[i]);
-            map.put("pick", false);
-            dataList.add(map);
+            mDataList.add(map);
         }
     }
-
 }
